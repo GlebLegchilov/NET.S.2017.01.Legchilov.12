0685005e186e30667286e1a6a181e43475700014
Gleb Legchilov <betrazen13@gmail.com>
2017-04-21 00:10:51 +0300

    Day12

---
+++ Task1/Queue.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Queue<T> : IEnumerable<T>
+    {
+
+
+        private T[] container;
+
+        public int Count { get { return container.Length; } }
+
+        private T this[int index]
+        {
+            get { return container[index]; }
+            set { container[index] = value; }
+        }
+
+        public Queue()
+        {
+
+            container = new T[0];
+        }
+
+        public Queue(T[] array)
+        {
+            container = new T[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                container[i] = array[i];
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (container.Length == 0) throw new InvalidOperationException();
+            T result = container[container.Length - 1];
+            Array.Resize<T>(ref container, container.Length - 1);
+            return result;
+        }
+
+        public void Enqueue(T item)
+        {
+
+            container[container.Length - 1] = item;
+        }
+
+        public bool Contains(T item)
+        {
+            return container.Contains<T>(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new Iterator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private struct Iterator : IEnumerator<T>
+        {
+            private readonly Queue<T> collection;
+            private int currentIndex;
+
+            public Iterator(Queue<T> collection)
+            {
+                currentIndex = collection.Count;
+                this.collection = collection;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    if (currentIndex == -1 || currentIndex == collection.Count)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    return collection[currentIndex];
+                }
+            }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+
+            public void Reset()
+            {
+
+                currentIndex = collection.Count;
+            }
+
+            public bool MoveNext()
+            {
+                return --currentIndex >= 0;
+            }
+
+            public void Dispose() { }
+        }
+    }
+}
---
+++ Task1Test/QueueTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task1;
+
+namespace Task1Test
+{
+    [TestFixture]
+    public class QueueTest
+    {
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, ExpectedResult = 5)]
+        public int? Queue_PositivTestSize(int[] array)
+        {
+            Task1.Queue<int> queue = new Task1.Queue<int>(array);
+            return queue.Count;
+        }
+
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 13 }, ExpectedResult = 13)]
+        public int? Queue_PositivTestLast(int[] array)
+        {
+            Task1.Queue<int> queue = new Task1.Queue<int>(array);
+            return queue.Dequeue();
+        }
+
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, ExpectedResult = 15)]
+        public int? Queue_PositivTestForeach(int[] array)
+        {
+            int result = 0;
+            Task1.Queue<int> queue = new Task1.Queue<int>(array);
+            foreach (var item in queue)
+                result += item;
+            return result;
+        }
+
+    }
+}
